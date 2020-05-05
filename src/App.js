2f15a06b54ced9a955cd64ecaf7ec964d9baa5a5
*abishek983* <*abishekwwe@gmail.com*>
2020-05-05 17:27:35 +0530

    home about added

--- src/App.js
+++ src/App.js
@@ -1,53 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
-import axios from 'axios';
 import Search from './components/users/Search';
 import Alert from './components/layouts/Alert';
+import About from './components/pages/About';
 
 class App extends Component {
   //we are not using any store management tool so getting the data from here
   state = {
-    users : [],
-    loading : false,
-    alert : null
+    users: [],
+    loading: false,
+    alert: null
   }
 
-  async componentDidMount(){
-    this.setState({loading : true});
+  /* async componentDidMount() {
+    this.setState({ loading: true });
     //we can also use the api without client ID and secret key
     // console.log(process.env.GITHUB_CLIENT_SECRET_KEY);
     const res = await axios.get(`https://api.github.com/users?client_id=${process.env.GITHUB_CLIENT_ID}&clientz-secret=${process.env.GITHUB_CLIENT_SECRET_KEY}`)
-    this.setState({loading : false});
-    this.setState({users : res.data});
-  }
+    this.setState({ loading: false });
+    this.setState({ users: res.data });
+  } */
 
   //Search Github Users
-  searchUsers = async(text) =>{
-    this.setState({loading : true});
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
     const res = await axios.get(`https://api.github.com/search/users?q=${text}`)
     // console.log(res.data);
-    this.setState({loading : false});
-    this.setState({users : res.data.items});
+    this.setState({ loading: false });
+    this.setState({ users: res.data.items });
   }
 
-  alert = (msg) =>{
-    this.setState({alert : {msg}})
-    setTimeout(() => this.setState({alert : null}),2000)
+  alert = (msg) => {
+    this.setState({ alert: { msg } })
+    setTimeout(() => this.setState({ alert: null }), 2000)
 
-  }  
+  }
 
   render() {
     return (
-      <div className="App">
-        <Navbar />
+      <Router>
+        <div className="App">
+          <Navbar />
           <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search searchUsers={this.searchUsers} alert={this.alert}/>
-          <Users users={this.state.users} loading={this.state.loading} searchUsers={this.searchUsers}/>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path='/'
+                render={props => (
+                  <Fragment>
+                    <Search searchUsers={this.searchUsers} alert={this.alert} />
+                    <Users users={this.state.users} loading={this.state.loading} searchUsers={this.searchUsers} />
+                  </Fragment>
+                )} />
+                <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PropTypes from 'prop-types';
 
-const Navbar = ({icon,title}) => {
+const Navbar = ({ icon, title }) => {
     /* // default props if no props is present this will be displayed
     static defaultProps = {
         title : "Github Finder",
@@ -18,20 +19,24 @@ const Navbar = ({icon,title}) => {
 
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light ">
-            <i className={icon}></i> <a className="navbar-brand" href="#">{title}</a>
+            <i className={icon}></i> <a className="navbar-brand" href="/">{title}</a>
+            <ul class="nav navbar-nav ml-auto">
+                <li><Link to="/"><span className="mx-2"></span>Home</Link></li>
+                <li><Link to="/about"><span className="mx-2"></span>About</Link></li>
+            </ul>
         </nav>
     )
 }
 
 //default props and proptypes for functional component
 Navbar.defaultProps = {
-    title : "Github Finder",
-    icon : "fab fa-github"
+    title: "Github Finder",
+    icon: "fab fa-github"
 };
 
-Navbar.propTypes  ={
-    title : PropTypes.string.isRequired,
-    icon : PropTypes.string.isRequired
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
 };
 
 export default Navbar;
